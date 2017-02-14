45c674cd4061159cb7ac22bbed6d62eacaf7cf9d
Asterart <peter.jacob.jeszke@gmail.com>
2017-02-14 11:23:11 +0100

    new title etc

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,25 +3,26 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
 	def setup
-		@base_title = "Lease your Energy"
+		@base_title = "Let's get to know each other"
+    @basic_title = "MeetMe"
 	end
 
   test "should get home" do
     get root_path
     assert_response :success
-    assert_select "title", "LyEn - Home | #{@base_title}"
+    assert_select "title", "#{@basic_title} - Home" + | + "#{base_title}"
   end
 
   test "should get help" do
     get help_path
     assert_response :success
-    assert_select "title", "LyEn - Help | #{@base_title}"
+    assert_select "title", "#{@basic_title} - Help" + | + "#{base_title}"
   end
 
   test "should get about" do
   	get about_path
   	assert_response :success
-  	assert_select "title", "LyEn - About us | #{@base_title}"
+  	assert_select "title", "#{@basic_title} - About us" + | + "#{base_title}"
   end
 
 end
